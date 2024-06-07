5c2db243f15fdd8ec5762cd227bb004bb33e6c66
5dwdvd <wudw.david@gmail.com>
2024-06-07 15:11:27 +0100

    Clean up molecular dynamics files

--- simulations/1_SingleProtein/HRT/job.sh
+++ simulations/1_SingleProtein/HRT/job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #PBS -N simulate_protein
 #PBS -l walltime=24:00:00
-#PBS -l select=1:ncpus=1:mem=16gb:ngpus=1:gpu_type=RTX6000
+#PBS -l select=1:ncpus=4:mem=16gb:ngpus=1:gpu_type=RTX6000
 
 source ~/.bashrc
 mamba activate md
 
-/rds/general/user/d21/home/iGEM/scripts/simulate_protein_implicit.py \
+/rds/general/user/d21/home/iGEM/scripts/simulate_protein.py \
 -i /rds/general/user/d21/home/iGEM/PDBs/HRT_Single.pdb \
 -o $(pwd) \
 -t 1000 \
